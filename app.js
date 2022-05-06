841bc101ead26043430c1d84460e6b71b906c444
I am Sheep <jorgexiaoyang@gmail.com>
2022-05-05 20:40:22 -0700

    added bin config and db folder

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
-const port = process.env.PORT || 8080;
+// require("dotenv").config();
+// const port = process.env.PORT || 8080;
 // console.log(port);
 
 app.get("/", (req, res) => {
   res.send("Working Server...");
 });
 
-app.listen(port || 8080, () => {
-  console.log(`Server is up....${port}!`);
-});
+module.exports = app;
