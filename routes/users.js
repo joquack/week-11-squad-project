a99ffeab86d0b687eabbd8df86e61de6f8191227
Chase Riddick <chaseriddick@outlook.com>
2022-05-09 13:17:52 -0700

    Test DB Created And Users route updated.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
+const db = require('../db/models');
 var router = express.Router();
+
+const { csrfProtection, asyncHandler } = require('./utils.js')
+const { check, validationResult } = require('express-validators');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+});
+
+router.get('/signup', function(req, res, next) {
+
+});
+
+router.post('/signup', function(req, res, next) {
+
+});
+
+router.get('/login', function(req, res, next) {
+
+});
+
+router.post('/login', function(req, res, next) {
+
 });
 
 module.exports = router;
