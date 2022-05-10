0e9ae7523be5351a05070e666e9a247e8f03a651
Chase Riddick <chaseriddick@outlook.com>
2022-05-10 09:44:01 -0700

    Progress on Question Routes & Views

--- routes/question.js
+++ routes/question.js
@@ -25,20 +25,93 @@ router.get("/create", csrfProtection, requireAuth, asyncHandler( async(req, res)
     res.render("question-create", {
         question,
         csrfToken: req.csrfToken(),
-        title: "Create a Question"
+        title: "Create Question"
     });
   })
 );
 
-router.post("/create", csrfProtection, requireAuth, asyncHandler( async(req, res) => {
+const questionValidators = [
+    check('title')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a value for Title')
+      .isLength({ max: 255 })
+      .withMessage('Title must not be more than 255 characters long'),
+    check('content')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a value for content'),
+  ];
+
+router.post("/create", csrfProtection, requireAuth, questionValidators, asyncHandler( async(req, res) => {
     const { title, content } = req.body;
-    const question = await db.Question.create({
+
+    const question = db.Question.build({
         title,
         content,
         userId: res.locals.user.id,
     });
-    res.redirect("/questions");
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+        await question.save();
+        res.redirect('/questions');
+      } else {
+        const errors = validatorErrors.array().map((error) => error.msg);
+        res.render('question-create', {
+          title: 'Create Question',
+          book,
+          errors,
+          csrfToken: req.csrfToken(),
+        });
+      }
   })
 );
+
+router.get('/edit/:id(\\d+)', requireAuth, csrfProtection,
+  asyncHandler(async (req, res) => {
+    const questionId = parseInt(req.params.id, 10);
+    const question = await db.Question.findByPk(questionId);
+
+    checkPermissions(question, res.locals.user);
+
+    res.render('question-edit', {
+      title: 'Edit Question',
+      book,
+      csrfToken: req.csrfToken(),
+    });
+  }));
+
+router.post('/edit/:id(\\d+)', requireAuth, csrfProtection, questionValidators,
+  asyncHandler(async (req, res) => {
+    const questionId = parseInt(req.params.id, 10);
+    const questionToUpdate = await db.Question.findByPk(questionId);
+
+    checkPermissions(book, res.locals.user);
+
+    const {
+      title,
+      content,
+    } = req.body;
+
+    const question = {
+        title,
+        content,
+    };
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      await questionToUpdate.update(question);
+      res.redirect('/questions');
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render('question-edit', {
+        title: 'Edit Question',
+        book: { ...question, questionId },
+        errors,
+        csrfToken: req.csrfToken(),
+      });
+    }
+  }));
 
 module.exports = router;
