3b7b4ef0cd47c9321fe5bf1fd42d8f8319c1019c
Chase Riddick <chaseriddick@outlook.com>
2022-05-11 10:38:52 -0700

    Updated use_en_variable value to correct mistake

--- config/database.js
+++ config/database.js
@@ -12,7 +12,7 @@ module.exports = {
     seederStorage: 'sequelize',
   },
   production: {
-    use_env_variable: 'postgres://mgznlatooiphfw:9c27cadf0eeca644cf78cfc09391d30d334a036290617da12a2ecb558571dac6@ec2-54-164-40-66.compute-1.amazonaws.com:5432/dclitbbthlnu36',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
   }
