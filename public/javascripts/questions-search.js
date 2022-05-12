e419f1131c02cd2e7d6b367f678f2033cf2d65fa
Jesse <jnjoroge13@gmail.com>
2022-05-12 15:52:32 -0400

    refactored questions search

--- public/javascripts/questions-search.js
+++ public/javascripts/questions-search.js
@@ -2,24 +2,29 @@ const searchBar = document.querySelector('.search-bar')
 const searchBtn = document.querySelector('.search-btn')
 const searchBarObject = document.getElementsByClassName('search-bar')
 const questionTitles = document.querySelectorAll('.question-title')
-const allQuestions = document.querySelectorAll('.question-list-card')
-searchBtn.addEventListener('click', e => {
-    for (let i = 0; i < allQuestions.length; i++){
+// const allQuestions = document.querySelectorAll('.question-list-card')
+const allQuestions1 = document.getElementsByClassName('question-list-card')
+const noQuestionsFound = document.getElementsByClassName('no-questions-found')
+// searchBtn.addEventListener('click', e => {
+//     for (let i = 0; i < allQuestions1.length; i++){
+//         if (!(questionTitles[i].innerHTML.includes(searchBarObject[0].value))) {
+//             allQuestions1[i].classList.add('hidden')
+//         }
+//         if (questionTitles[i].innerHTML.includes(searchBarObject[0].value)) {
+//             allQuestions1[i].classList.remove('hidden')
+//         }
+//     }
+// })
+searchBar.addEventListener('input', e => {
+    for (let i = 0; i < allQuestions1.length; i++){
         if (!(questionTitles[i].innerHTML.includes(searchBarObject[0].value))) {
-            allQuestions[i].style.display = 'none'
+            allQuestions1[i].classList.add('hidden')
         }
         if (questionTitles[i].innerHTML.includes(searchBarObject[0].value)) {
-            allQuestions[i].style.display = ''
+            allQuestions1[i].classList.remove('hidden')
         }
     }
-})
-searchBar.addEventListener('input', e => {
-    for (let i = 0; i < allQuestions.length; i++){
-        if (!(questionTitles[i].innerHTML.includes(searchBarObject[0].value))) {
-            allQuestions[i].style.display = 'none'
-        }
-        if (questionTitles[i].innerHTML.includes(searchBarObject[0].value)) {
-            allQuestions[i].style.display = ''
-        }
+    if (document.getElementsByClassName('hidden').length-1 == allQuestions1.length) {
+        noQuestionsFound.classList.remove('hidden')
     }
 })
