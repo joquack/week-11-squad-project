178835994ac907175ab21dd4588c34d1b65022f5
I am Sheep <jorgexiaoyang@gmail.com>
2022-05-12 14:33:31 -0700

    profile page load

--- routes/userRoutes/userprofile.js
+++ routes/userRoutes/userprofile.js
@@ -7,6 +7,7 @@ router.get("/users/:id(\\d+)/:userName", async (req, res) => {
   console.log(`*****`, Object.keys(req.signedCookies));
   const name = req.params.userName;
   let user = await db.User.findByPk(numId);
+  console.log(user.username, `!!!!!!!!!!!!!!`);
   try {
     console.log(user.username == name, user.username, name);
     if (
@@ -20,7 +21,15 @@ router.get("/users/:id(\\d+)/:userName", async (req, res) => {
       return res.render("profile-found-logged", { name });
     }
     if (user.username != name) {
-      return res.render("profile-not-found", { user: req.session.auth.userId });
+      console.log(`@@@@@@@HERE@@@@@@@@@@ first`);
+      // res.send("wtf are you doingß");
+      // return res.render(`not-logged`, {
+      //   name,
+      //   title: `${
+      //     name.charAt(0).toUpperCase() + name.slice(1)
+      //   }'s Profile | Stack Code`,
+      // });
+      return res.render("profile-not-found");
     }
     if (user.username == name && req.session.auth.userId != numId) {
       return res.render("limited-view", {
