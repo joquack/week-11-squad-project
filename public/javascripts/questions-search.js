06a0f2c725ed492ff755c8b0d86bc46a5f0c51e2
Jesse <jnjoroge13@gmail.com>
2022-05-12 15:09:31 -0400

    finished search bar for questions

--- public/javascripts/questions-search.js
+++ public/javascripts/questions-search.js
@@ -1,19 +1,25 @@
+const searchBar = document.querySelector('.search-bar')
 const searchBtn = document.querySelector('.search-btn')
 const searchBarObject = document.getElementsByClassName('search-bar')
-const questionTitle = document.querySelectorAll('.question-title')
+const questionTitles = document.querySelectorAll('.question-title')
 const allQuestions = document.querySelectorAll('.question-list-card')
 searchBtn.addEventListener('click', e => {
-    if (!(questionTitle[1].innerHTML.includes(searchBarObject[0].value))) {
-        allQuestions[1].style.display = 'none'
-    }
-    if (questionTitle[1].innerHTML.includes(searchBarObject[0].value)) {
-        allQuestions[1].style.display = ''
+    for (let i = 0; i < allQuestions.length; i++){
+        if (!(questionTitles[i].innerHTML.includes(searchBarObject[0].value))) {
+            allQuestions[i].style.display = 'none'
+        }
+        if (questionTitles[i].innerHTML.includes(searchBarObject[0].value)) {
+            allQuestions[i].style.display = ''
+        }
     }
 })
-
-// console.log(questionTitle[1].innerHTML)
-
-// allQuestions.forEach(question => {
-//     question.style.display = 'none'
-//     console.log(question)
-// })
+searchBar.addEventListener('input', e => {
+    for (let i = 0; i < allQuestions.length; i++){
+        if (!(questionTitles[i].innerHTML.includes(searchBarObject[0].value))) {
+            allQuestions[i].style.display = 'none'
+        }
+        if (questionTitles[i].innerHTML.includes(searchBarObject[0].value)) {
+            allQuestions[i].style.display = ''
+        }
+    }
+})
